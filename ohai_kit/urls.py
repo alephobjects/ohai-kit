c06757aceb2c35fec6b1acd87fd850dbedb1bd98
Aeva Palecek <aeva@alephobjects.com>
2013-11-20 16:10:07 -0600

    Added basic support for guest logins.

--- ohai_kit/urls.py
+++ ohai_kit/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 from ohai_kit import views
 
+
 urlpatterns = patterns(
     '',
     url(r'^$', views.system_index, name='index'),
@@ -11,4 +12,12 @@ urlpatterns = patterns(
     url(r'^jobs/(?P<job_id>\d+)/$', views.job_status, name='job_status'),
     url(r'^jobs/(?P<job_id>\d+)/close$', views.close_job, name='close_job'),
     url(r'^jobs/(?P<job_id>\d+)/update$', views.update_job, name='update_job'),
+
+    url(r'^guest_workflow/(?P<project_id>\d+)/$', views.guest_workflow, name='guest_workflow'),
+    url(r'accounts/guest_bypass/$', views.guest_access, name='guest_access'),
+
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', 
+        {'template_name': 'ohai_kit/login.html'}, name='login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', 
+        {'next_page': '/'}),
 )
--- ohai_kit/views.py
+++ ohai_kit/views.py
@@ -1,9 +1,18 @@
+try:
+    from urllib.parse import urlparse
+except ImportError:     # Python 2
+    from urlparse import urlparse
+
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, resolve_url
+from django.conf import settings
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.decorators import user_passes_test, login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.views import redirect_to_login
 from django.core.urlresolvers import reverse
 from django.utils import timezone
+from django.utils.encoding import force_str
 
 from ohai_kit.models import Project, JobInstance, \
     WorkStep, WorkReceipt
@@ -32,9 +41,69 @@ def get_active_jobs(user, project=None):
     return found
 
 
+def trigger_login_redirect(request, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
+    """
+    Code pulled from the auth module, because its nested, preventing reuse.
+    """
+    path = request.build_absolute_uri()
+    # urlparse chokes on lazy objects in Python 3, force to str
+    resolved_login_url = force_str(
+        resolve_url(login_url or settings.LOGIN_URL))
+    # If the login url is the same scheme and net location then just
+    # use the path as the "next" url.
+    login_scheme, login_netloc = urlparse(resolved_login_url)[:2]
+    current_scheme, current_netloc = urlparse(path)[:2]
+    if ((not login_scheme or login_scheme == current_scheme) and
+        (not login_netloc or login_netloc == current_netloc)):
+        path = request.get_full_path()
+    return redirect_to_login(
+        path, resolved_login_url, redirect_field_name)
 
 
-@login_required
+def guest_only(view_function, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
+    """
+    Replacement for @login_required decorator, so that only guests may
+    access the wrapped view.  In this event, the user is probably
+    "lost" so instead of redirecting to the login page, just redirect
+    to "/".\
+    """
+    def wrapped_view(request, *args, **kwargs):
+        if not request.user.is_authenticated() and \
+           request.session.has_key("bypass_login"):
+            return view_function(request, *args, **kwargs)
+        else:
+            # redirect to login page
+            return HttpResponseRedirect("/")
+    return wrapped_view
+
+
+def controlled_view(view_function, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
+    """
+    Replacement for the @login_required decorator that also takes in
+    account for if the session is an anonymous one.
+    """    
+    def wrapped_view(request, *args, **kwargs):
+        if request.user.is_authenticated() or \
+           request.session.has_key("bypass_login"):
+            return view_function(request, *args, **kwargs)
+        else:
+            # redirect to login page
+            return trigger_login_redirect(request, redirect_field_name, login_url)    
+    return wrapped_view
+
+
+def guest_access(request, redirect_field_name=REDIRECT_FIELD_NAME):
+    """
+    This view sets up a guest session and then redirects to wherever.
+    The session is set to expire when the browser closes, prompting
+    the login page next time they visit the site.
+    """
+    request.session.set_expiry(0)
+    request.session["bypass_login"] = True
+    return HttpResponseRedirect(request.POST[redirect_field_name])
+
+
+@controlled_view
 def system_index(request):
     """
     The dashboard view is the hub where the user is able to access
@@ -46,18 +115,25 @@ def system_index(request):
     context = {
         "projects" : projects,
         "user" : request.user,
+        "is_guest" : request.session.has_key("bypass_login"),
         }
     return render(request, "ohai_kit/dashboard.html", context)
 
 
-@login_required
+@controlled_view
 def project_view(request, project_id):
     """
     The Project view does different things for different people.  An
     administrator (eventually) should be able to edit Projects via the
     project view rather than the admin page.  For regular users, this
-    will be where they begin a workflow.
+    will be where they begin a workflow.  If the user is a guest user,
+    this should redirect right to the workflow view without a bound job.
     """
+
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(
+            reverse("ohai_kit:guest_workflow", args=(project_id,)))
+
 
     user = request.user
     project = get_object_or_404(Project, pk=project_id)
@@ -73,6 +149,48 @@ def project_view(request, project_id):
         }
 
     return render(request, "ohai_kit/project_detail.html", context)
+
+
+@guest_only
+def guest_workflow(request, project_id):
+    """
+    This view should facilitate the project workflow for guests.
+    """
+    project = get_object_or_404(Project, pk=project_id)
+    sequence = [[step, "pending"] for step in
+                project.workstep_set.order_by("sequence_number")]
+    if len(sequence) > 0:
+        # make the first step "active"
+        sequence[0][1] = "active"
+    context = {
+        "user": request.user,
+        "project": project,
+        "is_guest" : True,
+        "job_id": "-1",
+        "sequence": sequence,
+    }
+    return render(request, "ohai_kit/workflow.html", context)
+
+
+@login_required
+def job_status(request, job_id):
+    """
+    This view should facilitate the project workflow for the current
+    job.
+    """
+    job = get_object_or_404(JobInstance, pk=job_id)
+    # FIXME assert that the current user is either staff or the user listed
+    # on the job
+    assert request.user == job.user
+    
+    context = {
+        "user": request.user,
+        "project": job.project,
+        "is_guest" : False,
+        "job_id": job.pk,
+        "sequence": job.get_work_sequence(),
+    }
+    return render(request, "ohai_kit/workflow.html", context)
 
 
 @login_required
@@ -114,26 +232,6 @@ def close_job(request, job_id):
     else:
         return HttpResponseRedirect(
             reverse("ohai_kit:job_status", args=(job_id,)))
-
-
-@login_required
-def job_status(request, job_id):
-    """
-    This view should facilitate the project workflow for the current
-    job.
-    """
-    job = get_object_or_404(JobInstance, pk=job_id)
-    # FIXME assert that the current user is either staff or the user listed
-    # on the job
-    assert request.user == job.user
-
-    context = {
-        "user": request.user,
-        "project": job.project,
-        "job_id": job.pk,
-        "sequence": job.get_work_sequence(),
-        }
-    return render(request, "ohai_kit/workflow.html", context)
 
 
 @login_required
