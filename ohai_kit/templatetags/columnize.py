8a5415a703f3e3bfd596320d4e773bf31e90b981
Aeva Palecek <aeva@alephobjects.com>
2014-03-24 13:52:46 -0500

    columnize doesn't die when passed None

--- ohai_kit/templatetags/columnize.py
+++ ohai_kit/templatetags/columnize.py
@@ -7,6 +7,8 @@ def columnize(item_list, period):
     Take a list of items [1, 2, 3, 4, 5, 6, 7] and break it up into
     sub lists of some arbitrary period [[1, 2, 3, 4], [5, 6, 7]].
     """
+    if not item_list:
+        item_list = []
     columns = []
     ct = 0
     for item in item_list:
@@ -24,6 +26,8 @@ def remainderize(item_list, period):
     Generates a list of Nones to fill out the final row.  Used for
     evenly populating tables with data from columnize.
     """
+    if not item_list:
+        item_list = []
     columns = columnize(item_list, period)
     remainder = period - len(columns[-1])
     
